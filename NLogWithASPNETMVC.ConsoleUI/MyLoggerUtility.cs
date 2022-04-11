95718a9add57ede7e4d39819721a8c85e9b02055
Felix Arhawho <Felix.Arhawho@fcmb.com>
2022-04-11 15:20:57 +0100

    Nlog configuration

---
+++ NLogWithASPNETMVC.ConsoleUI/MyLoggerUtility.cs
@@ -0,0 +1,28 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogWithASPNETMVC.ConsoleUI
+{
+    public class MyLoggerUtility
+    {
+        private Logger _logger;
+        public MyLoggerUtility()
+        {
+            _logger = LogManager.GetCurrentClassLogger();
+        }
+
+        public void LogError()
+        {
+            _logger.Error("Logged Error");
+        }
+
+        public void LogInfo()
+        {
+            _logger.Info("Logged Info");
+        }
+    }
+}
---
+++ NLogWithASPNETMVC.ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogWithASPNETMVC.ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var loggerClass = new MyLoggerUtility();
+            loggerClass.LogInfo();
+            loggerClass.LogError();
+
+            Console.ReadLine();
+        }
+    }
+}
